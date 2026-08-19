8e3dc0f8f89f23cef43e4ae51fa896f1910aa462
Tim Wa Walter <tim.wa.walter-extern@deutschebahn.com>
2026-08-19 16:03:35 +0200

    Update dagre

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,9 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { optimizeCss } from 'carbon-preprocess-svelte';
-import type { Plugin } from 'vite';
-
-/**
- * HACK: Fixes issue with the CJS reference to @dagrejs/graphlib in @dagrejs/dagre@^2.
- * https://github.com/dagrejs/dagre/issues/492#issuecomment-3853652764
- */
-function dagreGraphlibPlugin(): Plugin {
-	return {
-		name: 'dagre-graphlib-plugin',
-		enforce: 'pre',
-		transform(code, id) {
-			if (!id.includes('dagrejs') || id.includes('graphlib')) return;
-			const modifiedCode = `
-        import * as __graphlib__ from '@dagrejs/graphlib';
-        ${code.replace(/g\(\s*["']@dagrejs\/graphlib["']\s*\)/g, '__graphlib__')}`;
-			return { code: modifiedCode, map: null };
-		}
-	};
-}
 
 export default defineConfig({
-	plugins: [sveltekit(), optimizeCss(), devtoolsJson(), dagreGraphlibPlugin()],
+	plugins: [sveltekit(), optimizeCss(), devtoolsJson()],
 	ssr: {
 		noExternal: process.env.NODE_ENV === 'production' ? ['sigma'] : []
 	},
